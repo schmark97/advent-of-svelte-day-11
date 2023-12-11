ab18aae7f290a250e65bc36fc1767b5704eaaa74
Mark <schuller.mark@thisisdone.com>
2023-12-11 14:40:09 +0100

    remove .env

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,9 +1,7 @@
-import { PUBLIC_API_URL } from '$env/static/public';
-
 
 export async function load({ fetch }) {
     try {
-        const response = await fetch(`${PUBLIC_API_URL}`);
+        const response = await fetch(`https://advent.sveltesociety.dev/data/2023/day-eleven.json`);
         const names = await response.json();
         return {
             names
